c85decb8edd319b7667a2c272523e168054d60b5
Takeshi <iwatakeshi@gmail.com>
2018-05-25 12:05:27 -0600

    Update: Change output in Section 3 - Factors.java

--- Chapter 1/Section 3/Factors.java
+++ Chapter 1/Section 3/Factors.java
@@ -7,12 +7,15 @@ public class Factors {
         // Print the prime factorization of n.
         long n = Long.parseLong(args[0]);
         for(long factor = 2; factor <= n/factor; factor++) {
+            System.out.printf("%s %-10d %s %-10d %s %-2d\n", "factor: ", factor, " n: ", n, " n / factor: ", (int) (n / factor));
             while (n % factor == 0) {
+                // Cast out and print factor.
                 n /= factor;
-                System.out.print(factor + " ");
+                // System.out.print(factor + " ");
             }
-            if (n > 1) System.out.print(n);
-            System.out.println();
+            // Any factor of n must be greater than factor.
         }
+        // if (n > 1) System.out.print(n);
+        // System.out.println();
     }
 }
