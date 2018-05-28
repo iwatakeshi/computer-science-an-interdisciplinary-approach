0f4001f9489148e0b91b8207baa75a5ef0cd9059
Takeshi <iwatakeshi@gmail.com>
2018-05-28 11:15:42 -0600

    Update: Add Section 5

---
+++ Chapter 1/Section 5/TwentyQuestions.java
@@ -0,0 +1,31 @@
+/**
+ * TwentyQuestions
+ * Program 1.5.2
+ * Interactive user input
+ */
+public class TwentyQuestions {
+    public static void main(String[] args) {
+        // Generate a number and answer questions
+        // while the user tries to guess the value.
+        int secret = 1 + (int) (Math.random() * 1000000);
+        StdOut.print("I'm thinking of a number ");
+        StdOut.println("between 1 and 1,000,000");
+        int guess = 0;
+        while (guess != secret) {
+            // Solicit on guess and provide one answer.
+            StdOut.print("What's your guess? ");
+            guess = StdIn.readInt();
+            if (guess == secret) {
+                StdOut.println("You win!");
+            }
+
+            if (guess < secret) {
+                StdOut.println("Too low");
+            }
+
+            if (guess > secret) {
+                StdOut.println("Too high");
+            }
+        }
+    }    
+}
