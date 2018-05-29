a47de35d9a4675cc37a26eba38f40c45601874e4
Takeshi <iwatakeshi@gmail.com>
2018-05-28 21:05:27 -0600

    Update: Complete Section 5

---
+++ Chapter 1/Section 5/BouncingBall.java
@@ -0,0 +1,29 @@
+/**
+ * BouncingBall
+ * Program 1.5.6
+ */
+public class BouncingBall {
+
+  public static void main(String[] args) {
+    // Simulate the motion of a bouncing ball.
+    StdDraw.setYscale(-1.0, 1.0);
+    StdDraw.setXscale(-1.0, 1.0);
+    StdDraw.enableDoubleBuffering();
+    double rx = 0.480, ry = 0.860;
+    double vx = 0.015, vy = 0.023;
+
+    double radius = 0.05;
+    while (true) {
+      // Update ball position and draw it.
+      if (Math.abs(rx + vx) + radius > 1.0) vx = -vx;
+      if (Math.abs(rx + vy) + radius > 1.0) vy = -vy;
+      rx += vx;
+      ry += vy;
+      StdDraw.clear();
+      StdDraw.filledCircle(rx, ry, radius);
+      StdDraw.show();
+      StdDraw.pause(20);
+    }
+  }
+  
+}
---
+++ Chapter 1/Section 5/PlayThatTune.java
@@ -0,0 +1,22 @@
+/**
+ * PlayThatTune
+ * Program 1.5.7
+ */
+public class PlayThatTune {
+  public static void main(String[] args) {
+    // Read a tune from Stdin and play it.
+    int SAMPLING_RATE = 44100;
+    while (!StdIn.isEmpty()) {
+      // Read and play one note.
+      int pitch = StdIn.readInt();
+      double duration = StdIn.readDouble();
+      double hz = 440 * Math.pow(2, pitch / 12.0);
+      int n = (int) (SAMPLING_RATE * duration);
+      double[] a = new double[n + 1];
+      for (int i = 0; i < n; i++) {
+        a[i] = Math.sin(2 * Math.PI * i * hz / SAMPLING_RATE);
+      }
+      StdAudio.play(a);
+    }
+  }
+}
